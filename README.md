fec27ccfe02554ea3b682424d5162e6bd30a66ac
marmooo <marmooo@users.noreply.github.com>
2021-09-11 17:19:36 +0900

    deno lint

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # 手書き漢字検索
+
 [手書きで漢字検索](https://marmooo.github.io/tegaki-search/)します。
 
+## Build
+
+```
+bash build.sh
+```
--- src/sw.js
+++ src/sw.js
@@ -1,51 +1,50 @@
-var CACHE_NAME = '2021-08-09 00:08';
+var CACHE_NAME = "2021-09-11 17:15";
 var urlsToCache = [
-  '/tegaki-kanji-search/',
-  '/tegaki-kanji-search/worker.js',
-  '/tegaki-kanji-search/index.js',
-  '/tegaki-kanji-search/favicon/original.svg',
-  '/tegaki-kanji-search/demo.json',
-  '/tegaki-kanji-search/model/model.json',
-  '/tegaki-kanji-search/model/group1-shard1of1.bin',
-  '/tegaki-kanji-search/signature_pad.umd.min.js',
-  'https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js',
+  "/tegaki-kanji-search/",
+  "/tegaki-kanji-search/worker.js",
+  "/tegaki-kanji-search/index.js",
+  "/tegaki-kanji-search/favicon/original.svg",
+  "/tegaki-kanji-search/demo.json",
+  "/tegaki-kanji-search/model/model.json",
+  "/tegaki-kanji-search/model/group1-shard1of1.bin",
+  "/tegaki-kanji-search/signature_pad.umd.min.js",
+  "https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs/dist/tf.min.js",
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
     caches
-    .open(CACHE_NAME)
-    .then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
+      .open(CACHE_NAME)
+      .then(function (cache) {
+        return cache.addAll(urlsToCache);
+      }),
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+      .then(function (response) {
         if (response) {
           return response;
         }
         return fetch(event.request);
-      }
-    )
+      }),
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener("activate", function (event) {
   var cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
+        cacheNames.map(function (cacheName) {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
-        })
+        }),
       );
-    })
+    }),
   );
 });
