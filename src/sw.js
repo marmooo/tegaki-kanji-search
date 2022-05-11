0fa4bee53f6fc9c88e2565edac0f406c4ee74d2f
marmooo <marmooo@users.noreply.github.com>
2022-05-11 09:31:30 +0900

    add OGP

--- src/sw.js
+++ src/sw.js
@@ -1,15 +1,15 @@
-var CACHE_NAME = '2022-04-23 08:06';
+var CACHE_NAME = "2022-05-11 09:30";
 var urlsToCache = [
   "/tegaki-kanji-search/",
-  "/tegaki-kanji-search/worker.js",
   "/tegaki-kanji-search/index.js",
-  "/tegaki-kanji-search/favicon/original.svg",
+  "/tegaki-kanji-search/worker.js",
   "/tegaki-kanji-search/demo.json",
   "/tegaki-kanji-search/model/model.json",
   "/tegaki-kanji-search/model/group1-shard1of1.bin",
   "/tegaki-kanji-search/eraser.svg",
-  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.4/dist/signature_pad.umd.min.js",
+  "/tegaki-kanji-search/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+  "https://cdn.jsdelivr.net/npm/signature_pad@4.0.4/dist/signature_pad.umd.min.js",
   "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@3.15.0/dist/tf.min.js",
 ];
 
