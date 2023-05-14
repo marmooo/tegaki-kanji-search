a4218d5570bd138b19353aff665f3452888de20b
marmooo <marmooo@users.noreply.github.com>
2023-05-14 15:07:50 +0900

    bump tfjs from 4.4.0 to 4.5.0

--- src/sw.js
+++ src/sw.js
@@ -1,4 +1,4 @@
-var CACHE_NAME = "2023-04-23 10:04";
+var CACHE_NAME = "2023-05-14 15:07";
 var urlsToCache = [
   "/tegaki-kanji-search/",
   "/tegaki-kanji-search/index.js",
@@ -10,7 +10,7 @@ var urlsToCache = [
   "/tegaki-kanji-search/favicon/favicon.svg",
   "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/signature_pad@4.1.5/dist/signature_pad.umd.min.js",
-  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.4.0/dist/tf.min.js",
+  "https://cdn.jsdelivr.net/npm/@tensorflow/tfjs@4.5.0/dist/tf.min.js",
 ];
 
 self.addEventListener("install", function (event) {
